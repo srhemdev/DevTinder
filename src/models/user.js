a0988ac1b015aa0ae228a69a52b7f910e6378d49
Shweta Hemdev <srhemdev@Shwetas-MacBook-Pro.local>
2026-08-23 12:59:35 -0700

    Authentication, JWT & cookies

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator"); // Importing the validator library for email validation
 
 const userSchema = mongoose.Schema({
   firstName: {
@@ -15,10 +16,20 @@ const userSchema = mongoose.Schema({
     required: true,
     unique: true,
     trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Please enter a valid email address");
+      }
+    }
   },
   password: {
     type: String,
     required: true,
+    validate(value) {
+      if (!validator.isStrongPassword(value)) {
+        throw new Error("Password must be a strong password");
+      }
+    }
   },
   age: {
     type: Number,
@@ -34,7 +45,12 @@ const userSchema = mongoose.Schema({
   },
   photoUrl: {
     type: String,
-    default: "https://img.magnific.com/premium-photo/silhouette-adult-woman-female-avatar-social-media-icon-illustration-isolated_314149-11966.jpg"
+    default: "https://img.magnific.com/premium-photo/silhouette-adult-woman-female-avatar-social-media-icon-illustration-isolated_314149-11966.jpg",
+    validate(value) {
+      if (!validator.isURL(value)) {
+        throw new Error("Please enter a valid URL for the photo");
+      }
+    }
   },
   about: {
     type: String,
